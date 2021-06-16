00bc53406859772a161c89f59c4c109c7788239b
PoshMonger <85910267+PoshMonger@users.noreply.github.com>
2021-06-15 22:53:43 -0400

    new

---
+++ newcss.js
@@ -0,0 +1,79 @@
+
+
+
+function newcss(){
+
+var elements = document.getElementsByClassName('gamelink');
+for (var i in elements) {
+  elements[i].style.cssText = "display: inline-block;color:red; background-color:coral; padding: 14px 25px;text-align: left;text-decoration: none;display: inline-block;margin-bottom: 20px;"
+  
+
+}
+
+}
+
+
+function newcss2(){
+
+   var elements2 = document.getElementsByClassName('gamelink');
+   for (var i in elements2) {
+     elements2[i].style.cssText = "color:white; background-color:black; padding: 14px 25px;text-align: left;text-decoration: none;display: inline-block;margin-bottom: 20px;"
+     
+   
+   }
+   
+   }
+
+
+
+
+   function newcss3(){
+
+      var elements3 = document.getElementsByClassName('gamelink');
+      for (var i in elements3) {
+        elements3[i].style.cssText = "color:blue; background-color:lightblue; padding: 14px 25px;text-align: left;text-decoration: none;display: inline-block;margin-bottom: 20px;"
+        
+      
+      }
+      
+      }
+      
+      
+      function newcss4(){
+      
+         var elements4 = document.getElementsByClassName('gamelink');
+         for (var i in elements4) {
+           elements4[i].style.cssText = "color:black; background-color:lightslategrey; padding: 14px 25px;text-align: left;text-decoration: none;display: inline-block;margin-bottom: 20px;"
+           
+         
+         }
+         
+         }
+      
+
+
+
+
+         function newcss5(){
+
+            var elements5 = document.getElementsByClassName('gamelink');
+            for (var i in elements5) {
+              elements5[i].style.cssText = "color:green; background-color:lightgreen; padding: 14px 25px;text-align: left;text-decoration: none;display: inline-block;margin-bottom: 20px;"
+              
+            
+            }
+            
+            }
+            
+            
+            function newcss6(){
+            
+               var elements6 = document.getElementsByClassName('gamelink');
+               for (var i in elements6) {
+                 elements6[i].style.cssText = "color:white; background-color:purple; padding: 14px 25px;text-align: left;text-decoration: none;display: inline-block;margin-bottom: 20px;"
+                 
+               
+               }
+               
+               }
+            
